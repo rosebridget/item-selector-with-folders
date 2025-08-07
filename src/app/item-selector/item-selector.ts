c4d2bd888a8bf90d4ac8ecb9ccb592923fd7e802
Rose <contact@rosebridget.com>
2025-08-07 17:56:14 +0200

    use equipment service in item selector and add folder row to template

--- src/app/item-selector/item-selector.ts
+++ src/app/item-selector/item-selector.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { FolderRow } from '../folder-row/folder-row';
+import { Folder } from '../models/folder';
+import { Equipment as EquipmentService } from '../services/equipment';
 
 @Component({
   selector: 'app-item-selector',
-  imports: [],
+  imports: [FolderRow],
   templateUrl: './item-selector.html',
-  styleUrl: './item-selector.css'
+  styleUrl: './item-selector.css',
 })
-export class ItemSelector {
+export class ItemSelector implements OnInit {
+  @Input() selectedIds!: Set<number>;
+  folders: Folder[] = [];
+  equipmentService = inject(EquipmentService);
 
+  ngOnInit() {
+    this.equipmentService.getAllItemSelectorData().subscribe((folders) => {
+      this.folders = folders;
+    });
+  }
 }
